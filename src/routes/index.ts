7d6dec5de3ae2bda18147e0dea1aff159eb12292
Yuri Nezdemkovski <yuri@nezdemkovski.com>
2025-04-18 20:07:37 +0200

    feat: add game update endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,7 @@ import { getGamesSearch } from "./games/search/GET";
 import { getUserGames } from "./user/[username]/games/GET";
 import { postUserGame } from "./user/[username]/games/POST";
 import { deleteUserGame } from "./user/[username]/games/[id]/DELETE";
+import { patchUserGame } from "./user/[username]/games/[id]/PATCH";
 
 export const routes = new Elysia()
   .use(getMe)
@@ -22,4 +23,5 @@ export const routes = new Elysia()
 
   .use(getUserGames)
   .use(postUserGame)
+  .use(patchUserGame)
   .use(deleteUserGame);
